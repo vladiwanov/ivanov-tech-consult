9ccd3c64c6daf43599c0c1ad53f41736fa886eab
vladiwanov <3298901@gmail.com>
2023-11-13 14:21:27 +0200

    removed A/V navigation

--- src/db/en/navigation-en.js
+++ src/db/en/navigation-en.js
@@ -2,7 +2,7 @@ const navigation = [
   { title: 'Why us', to: '#benefits' },
   { title: 'Information systems', to: '#inform' },
   { title: 'Security systems', to: '#security' },
-  { title: 'Audio-video', to: '#av' },
+  // { title: 'Audio-video', to: '#av' },
   { title: 'Electric', to: '#EL' },
   { title: 'Natural Energy', to: '#altenergy' },
   { title: 'Consulting', to: '#consulting' },
--- src/db/ua/navigation-ua.js
+++ src/db/ua/navigation-ua.js
@@ -2,7 +2,7 @@ const navigation = [
   { title: 'Чому ми', to: '#benefits' },
   { title: 'інформаційні системи', to: '#inform' },
   { title: 'системи безпеки', to: '#security' },
-  { title: 'Аудіо-відео', to: '#av' },
+  // { title: 'Аудіо-відео', to: '#av' },
   { title: 'Електрика', to: '#EL' },
   { title: 'альтернативна енергетика', to: '#altenergy' },
   { title: 'Консалтингова діяльність', to: '#consult' },
